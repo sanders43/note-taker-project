44d4cb02c4d87e3ee4a0fde0564a5463b52b0b40
Waves <64664814+sanders43@users.noreply.github.com>
2020-08-16 23:04:30 -0500

    get html working

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+//const apiRoutes = require("./routes/apiRoutes")
+const htmlRoutes = require("./routes/htmlRoutes")
+const fs = require("fs")
+const path = require("path")
+const { notes } = require("./db/db")
 const express = require("express")
 const PORT = process.env.Port || 3001
 const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(express.json());
+
+//app.use("/api", apiRoutes);
+
+app.use("/", htmlRoutes);
+
+app.use(express.static("public"));
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`)
 })
