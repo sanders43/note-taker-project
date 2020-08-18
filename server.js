30a282f728be5cc28ca624094092f0dbc0b8d84d
Waves <64664814+sanders43@users.noreply.github.com>
2020-08-17 22:08:28 -0500

    whoops

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const htmlRoutes = require("./routes/htmlRoutes")
 const apiRoutes = require("./routes/apiRoutes")
-
+const htmlRoutes = require("./routes/htmlRoutes")
 const fs = require("fs")
 const path = require("path")
 const { notes } = require("./db/db")
 const express = require("express")
-const PORT = process.env.Port || 3002;
 const app = express();
+const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true}));
 
